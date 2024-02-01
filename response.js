7da33c0c63fee6a80752826b88546c293441d0f9
MohamedCodeHub <abdullahabd4321@gmail.com>
2024-02-01 18:15:25 +0530

    commit

--- response.js
+++ response.js
@@ -2,6 +2,6 @@ const responseObj = {
   hello: "Hey ! How are you doing ?",
   hey: "Hey! What's Up",
   bye:"great,if u want anything just ping me",
-  today: new Date().toDateString(),
+  date: new Date().toDateString(),
   time: new Date().toLocaleTimeString(),
 };
